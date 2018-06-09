bae87cae6a3583deaaff8309d5ccc1fd1d2fb878
Evan B <admin@storm-east.com>
2018-06-09 06:17:32 -0600

    Created join app

--- social/urls.py
+++ social/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from . import views
+from django.conf.urls import url
+from joins import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
+    url('', views.home, name='home')
 ]
--- social/views.py
+++ social/views.py
@@ -1,9 +1,2 @@
 from django.shortcuts import render
 
-
-def home(request):
-    context = {
-        "title": 'Welcome'
-    }
-
-    return render(request, 'home.html', context)
